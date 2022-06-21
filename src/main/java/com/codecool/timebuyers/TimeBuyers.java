263992e60ec058549a22966c27f3fc6f4393ae72
takacsberni <takacsberni@gmail.com>
2022-06-21 11:04:20 +0200

    feat: create a Spring project

---
+++ src/main/java/com/codecool/timebuyers/TimeBuyers.java
@@ -0,0 +1,13 @@
+package com.codecool.timebuyers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeBuyers {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeBuyers.class, args);
+    }
+
+}
