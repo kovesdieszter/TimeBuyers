7e2f9df7166adc5f91e2141ecc986a0057afd018
kovesdieszter <kovesdieszter@github.com>
2022-06-22 10:15:37 +0200

    feat: implement user profile model

--- src/main/java/com/codecool/timebuyers/model/UserProfile.java
+++ src/main/java/com/codecool/timebuyers/model/UserProfile.java
@@ -1,4 +1,20 @@
 package com.codecool.timebuyers.model;
 
+import java.util.UUID;
+
 public class UserProfile {
+    private UUID id;
+    private String userName;
+    private String password;
+    private String phoneNumber;
+    private String email;
+    private UserStatus userStatus;
+    private Set<Task> taskToNeed;
+    private Set<Task> taskToTake;
+
+    public UserProfile(UUID id, String userName, UserStatus userStatus) {
+        this.id = UUID.randomUUID();
+        this.userName = userName;
+        this.userStatus = userStatus;
+    }
 }
