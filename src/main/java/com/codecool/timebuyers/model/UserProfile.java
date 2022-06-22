e91da1b9b59052d11e7d9d8912a1f393a0d9a4c5
kovesdieszter <kovesdieszter@github.com>
2022-06-22 10:50:50 +0200

    feat: add necessary getters and setters

--- src/main/java/com/codecool/timebuyers/model/UserProfile.java
+++ src/main/java/com/codecool/timebuyers/model/UserProfile.java
@@ -8,13 +8,51 @@ public class UserProfile {
     private String password;
     private String phoneNumber;
     private String email;
+    private String town;
     private UserStatus userStatus;
     private Set<Task> taskToNeed;
     private Set<Task> taskToTake;
 
-    public UserProfile(UUID id, String userName, UserStatus userStatus) {
+    public UserProfile(UUID id, String userName, String password, UserStatus userStatus) {
         this.id = UUID.randomUUID();
         this.userName = userName;
+        this.password = password;
         this.userStatus = userStatus;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public Set<Task> getTaskToNeed() {
+        return taskToNeed;
+    }
+
+    public Set<Task> getTaskToTake() {
+        return taskToTake;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public void setUserStatus(UserStatus userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public void setTaskToNeed(Set<Task> taskToNeed) {
+        this.taskToNeed = taskToNeed;
+    }
+
+    public void setTaskToTake(Set<Task> taskToTake) {
+        this.taskToTake = taskToTake;
     }
 }
