83aab8ea0fcce27bd4bb37dded602a7cd5e8bf84
takacsberni <takacsberni@gmail.com>
2022-06-22 19:55:06 +0200

    feat: logic of getResultList - in case of pay with task - is done

--- src/main/java/com/codecool/timebuyers/payment/PayWithTask.java
+++ src/main/java/com/codecool/timebuyers/payment/PayWithTask.java
@@ -15,7 +15,16 @@ public class PayWithTask implements Pay {
     public Set<UserProfile> getResultList(UserProfile buyer, UserProfile tasker, Task taskToBuy, Task taskToOffer, UserStorageService userStorageService) {
         Set<UserProfile> resultList = new HashSet<>();
 
-
+        Set<UserProfile> allUsers = userStorageService.getAllUser();
+        for (UserProfile user : allUsers){
+            if (user.getUserStatus().equals(UserStatus.TASKER) || user.getUserStatus().equals(UserStatus.BOTH)){
+                if (user.getTown().equals(buyer.getTown())){
+                    if (user.getTaskToNeed().contains(taskToOffer)){ //returns with boolean
+                        resultList.add(user);
+                    }
+                }
+            }
+        }
 
         return resultList;
     }
