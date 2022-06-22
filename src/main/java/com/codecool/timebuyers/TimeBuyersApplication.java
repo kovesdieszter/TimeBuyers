44170880f538340114c78e0ca091c5744787954a
takacsberni <takacsberni@gmail.com>
2022-06-22 22:19:59 +0200

    feat: makes a test in main method

--- src/main/java/com/codecool/timebuyers/TimeBuyersApplication.java
+++ src/main/java/com/codecool/timebuyers/TimeBuyersApplication.java
@@ -1,5 +1,12 @@
 package com.codecool.timebuyers;
 
+import com.codecool.timebuyers.factory.PaymentFactory;
+import com.codecool.timebuyers.model.PaymentMethod;
+import com.codecool.timebuyers.model.Task;
+import com.codecool.timebuyers.model.UserProfile;
+import com.codecool.timebuyers.model.UserStatus;
+import com.codecool.timebuyers.service.HireService;
+import com.codecool.timebuyers.service.UserStorageService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +15,29 @@ public class TimeBuyersApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TimeBuyersApplication.class, args);
+        UserStorageService userStorageService = new UserStorageService();
+
+        UserProfile buyer = new UserProfile("Buyer1", "password", UserStatus.BUYER);
+        buyer.addTaskToNeed(Task.COOKING);
+        buyer.addTaskToNeed(Task.CAT_SITTING);
+        buyer.addTaskToNeed(Task.GARDENING);
+        buyer.setTown("Budapest");
+        UserProfile tasker = new UserProfile("Tasker1", "taskerPassword", UserStatus.TASKER);
+        tasker.addTaskToTake(Task.GARDENING);
+        tasker.setTown("Budapest");
+        UserProfile both = new UserProfile("Both1", "bothPassword", UserStatus.BOTH);
+        both.addTaskToTake(Task.GARDENING);
+        both.addTaskToNeed(Task.COOKING);
+        both.setTown("Budapest");
+
+        userStorageService.addUser(buyer);
+        userStorageService.addUser(tasker);
+        userStorageService.addUser(both);
+
+        HireService hireService = new HireService(new PaymentFactory(), buyer, Task.GARDENING, PaymentMethod.CASH);
+        hireService.setPossibleTaskers(userStorageService);
+        System.out.println(hireService.getPossibleTaskers());
+        hireService.setTasker(tasker);
+        hireService.sendEmail();
     }
 }
