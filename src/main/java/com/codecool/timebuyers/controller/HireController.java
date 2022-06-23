d7acec05a2c0a48c6abc3bf0b10f501ef3791711
takacsberni <takacsberni@gmail.com>
2022-06-23 14:44:27 +0200

    feat: setTasker method is available through api/settasker

--- src/main/java/com/codecool/timebuyers/controller/HireController.java
+++ src/main/java/com/codecool/timebuyers/controller/HireController.java
@@ -4,9 +4,7 @@ import com.codecool.timebuyers.factory.PaymentFactory;
 import com.codecool.timebuyers.model.UserProfile;
 import com.codecool.timebuyers.service.HireService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,10 @@ public class HireController {
     @GetMapping(value="api/getPossibleTaskers")
     public List<UserProfile> getPossibleTaskers(){
         return hireService.getPossibleTaskers();
+    }
+
+    @PostMapping(value = "api/settasker")
+    public void setTasker(@RequestBody UserProfile tasker) {
+        hireService.setTasker(tasker);
     }
 }
