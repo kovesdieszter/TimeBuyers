d5848eef14c5f078b30207a147aaf7ee8266ef6b
takacsberni <takacsberni@gmail.com>
2022-07-19 22:18:28 +0200

    feat: merge navbar and search bar into home component

--- src/frontend/timebuyers_app/src/components/Home.js
+++ src/frontend/timebuyers_app/src/components/Home.js
@@ -2,15 +2,17 @@ import React from "react";
 import '../static/CSS/Home.css';
 import Inside from "./Inside";
 import Footer from "./Footer";
+import Searchbar from "./Searchbar";
+import Navbar from "./Navbar";
 
 function Home() {
     return (
         <body>
         <div className="flex-container-home">
-            <div className="navbar">Navbar's place</div>
-            <div className="searchbar">Searchbar's place</div>
-            <div className="inside">Inside's place <Inside /> </div>
-            <div className="footer">Footer's place <Footer /></div>
+            <div className="navbar"> <Navbar /> </div>
+            <div className="searchbar"> <Searchbar/> </div>
+            <div className="inside"> <Inside /> </div>
+            <div className="footer"> <Footer /></div>
         </div>
         </body>
     )
