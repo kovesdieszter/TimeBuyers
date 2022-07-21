177f60898dd6d9e73f58fad23afc253309cf299b
Hanna Szepesi <hannaszepesi.7@gmail.com>
2022-07-21 11:21:17 +0200

    fix: fix footer layout

--- src/frontend/timebuyers_app/src/components/Footer.js
+++ src/frontend/timebuyers_app/src/components/Footer.js
@@ -7,20 +7,20 @@ function Footer() {
         <footer>
             <div className="flex-container-footer">
                 <div className="footer-col-1">
-                    <h5>TimeBuyers</h5>
-                    <h5>2022</h5>
-                    <h5>CodeCool</h5>
+                    <h5 className="text-md-left">TimeBuyers</h5>
+                    <h5 className="text-md-left">2022</h5>
+                    <h5 className="text-md-left">CodeCool</h5>
                 </div>
                 <div className="footer-col-2">
                     <ul className="list-us">
-                        <h5>Eszter Kövesdi</h5>
-                        <h5>Hanna Szepesi</h5>
-                        <h5>Bernadett Takács</h5>
+                        <h5 className="middle">Eszter Kövesdi</h5>
+                        <h5 className="middle">Hanna Szepesi</h5>
+                        <h5 className="middle">Bernadett Takács</h5>
                     </ul>
                 </div>
                 <div className="footer-col-3">
                     <h5 className="text-md-right">Contact Us</h5>
-                    <h5> cctimebuyers@gmail.com </h5>
+                    <h5 className="text-md-right"> cctimebuyers@gmail.com </h5>
                 </div>
             </div>
         </footer>
