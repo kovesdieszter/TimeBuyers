31ee53955fb6241ca43de6cd13c6310d76a207a7
kovesdieszter <kovesdieszter@github.com>
2022-07-21 14:00:25 +0200

    feat: add user card jsx

--- src/frontend/timebuyers_app/src/routes/AllUsers.js
+++ src/frontend/timebuyers_app/src/routes/AllUsers.js
@@ -1,7 +1,10 @@
 import React, {useEffect, useState} from "react";
+import '../static/CSS/Usercard.css';
+import '../static/CSS/Inside.css';
+import test_pic from '../static/images/testphoto.jpg'
 
-const fetchAllUsers = async() => {
-    const response = await(fetch("/api/users"));
+const fetchAllUsers = async () => {
+    const response = await (fetch("/api/users"));
     return await response.json();
 }
 
@@ -12,7 +15,49 @@ export default function AllUsers() {
             setUsers(users)
         })
     }, [])
+    return  (<div className="cardContainer">
+        {showUserCards(users)}
+    </div>)
 
-
-    return(<pre>{JSON.stringify(users, null, 2)}</pre>)
 }
+
+const showUserCards = function (data) {
+    console.log(data.map(item => (
+        item
+    )));
+    return data.map(item => (
+        <UserCard
+            name={item.userName}
+            town={item.town}
+            email={item.email}
+            taskNeed={item.taskToNeed}
+            taskTake={item.taskToTake}
+        />));
+
+}
+
+const UserCard = (item) => {
+    return (
+                <div className="card">
+                    <img src={test_pic} alt="Test" style={{width: "100%"}}/>
+                    <h1>{item.name}</h1>
+                    <p className="title">{item.email}</p>
+                    <p>{item.town}</p>
+                    <p>NEED: {item.taskNeed.map((x, index) => {
+                        if(index === item.taskNeed.length-1)
+                            return x.replace("_", "-");
+                        else return x.replace("_", "-") + ", ";
+                    })}</p>
+                    <p>TAKE: {item.taskTake.map((x, index) => {
+                        if(index === item.taskTake.length-1)
+                        return x.replace("_", "-");
+                        else return x.replace("_", "-") + ", ";
+                    })}</p>
+                    <p>
+                        <button>Hire</button>
+                    </p>
+                </div>
+    )
+        ;
+}
+
