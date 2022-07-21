e952bec9508b65c543903c8da59b8890feaca745
takacsberni <takacsberni@gmail.com>
2022-07-21 12:06:54 +0200

    feat: make a link from "TimeBuyers" logo to the home page

--- src/frontend/timebuyers_app/src/components/Navbar.js
+++ src/frontend/timebuyers_app/src/components/Navbar.js
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 function Navbar(){
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand">TimeBuyers</a>
+            <Link to={"/"}  className="navbar-brand">TimeBuyers</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
