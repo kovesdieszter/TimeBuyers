4ee8beb87839bddc9068006749df279f5163d82c
takacsberni <takacsberni@gmail.com>
2022-07-21 18:02:36 +0200

    feat: use states in contact us

--- src/frontend/timebuyers_app/src/components/ContactUs.js
+++ src/frontend/timebuyers_app/src/components/ContactUs.js
@@ -5,18 +5,64 @@ const ENDPOINT = ""; // TODO - ide jöjjön majd a route!
 
 function ContactUs() {
 
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const [submitted, setSubmitted] = useState(false);
+    const [error, setError] = useState(false);
+
+    const handleName = (e) => {
+        setName(e.target.value);
+    };
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handleMessage = (e) => {
+        setMessage(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (name === '' || email === '') {
+            setError(true);
+        } else {
+            setSubmitted(true);
+            setError(false);
+        }
+    };
+
+    const successMessage = () => {
+        return (
+            <div
+                className="success"   style={{display: submitted ? '' : 'none'}}>
+                <h1>{name}, your message has been successfully sent!!</h1>
+            </div>
+        );
+    };
+
+    const errorMessage = () => {
+        return (
+            <div className="error"  style={{display: error ? '' : 'none'}}>
+                <h1>Please enter all the fields</h1>
+            </div>
+        );
+    };
+
 
     return (
         <div className="container">
-            <form action="">
-                <label htmlFor="fname">First Name</label>
-                <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+            <form action="api/send-email/takacsberni@gmail.com">
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" name="name" placeholder="Your name.." onChange={handleName} value={name} type="text"/>
 
-                <label htmlFor="lname">Last Name</label>
-                <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" name="email" placeholder="Your email address.." onChange={handleEmail} value={email} type="text" />
 
-                <label htmlFor="subject">Subject</label>
-                <textarea id="subject" name="subject" placeholder="White a message...">  </textarea>
+                <label htmlFor="message">Message</label>
+                <textarea id="message" name="message" placeholder="White a message..." onChange={handleMessage} value={message} type="text">  </textarea>
 
                 <input type="submit" value="Submit"/>
             </form>
