1c16993a80a86a1000cee009d8908b00503e6a4d
takacsberni <takacsberni@gmail.com>
2022-08-02 11:32:14 +0200

    feat: add findByUSerStatus method

--- src/backend/main/java/com/codecool/timebuyers/dao/UserStorageRepository.java
+++ src/backend/main/java/com/codecool/timebuyers/dao/UserStorageRepository.java
@@ -1,6 +1,7 @@
 package com.codecool.timebuyers.dao;
 
 import com.codecool.timebuyers.model.UserProfile;
+import com.codecool.timebuyers.model.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
@@ -8,4 +9,5 @@ import java.util.UUID;
 public interface UserStorageRepository extends JpaRepository<UserProfile, UUID> {
     UserProfile findByEmail(String email);
     UserProfile findByUserName(String username);
+    UserProfile findByUserStatus(UserStatus userStatus);
 }
