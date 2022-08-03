f9984873bc972ebbbe96bbfa83e0517e5f5d1e06
Hanna Szepesi <hannaszepesi.7@gmail.com>
2022-08-03 22:25:12 +0200

    feat: update uppersection with different pics

--- src/frontend/timebuyers_app/src/components/UpperSection.js
+++ src/frontend/timebuyers_app/src/components/UpperSection.js
@@ -1,28 +1,27 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect} from "react";
 import '../static/CSS/UpperSection.css';
-import { useTransition, animated, config } from 'react-spring'
 
 function UpperSection() {
 
-    const first = <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
+    const first = <section id="hero1" className="d-flex flex-column justify-content-center align-items-center">
         <div className="upper-section-text-container" data-aos="fade-up"><h1>Welcome to <span>TimeBuyers</span></h1>
             <h2>Welcome to our webpage, where you can either buy or sell some time!</h2></div>
     </section>
 
-    const second = <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
-        <div className="upper-section-text-container" data-aos="fade-up"><h1>Welcome to <span>Second</span></h1>
-            <h2>Welcome to our webpage, where you can either buy or sell some time!</h2></div>
+    const second = <section id="hero2" className="d-flex flex-column justify-content-center align-items-center">
+        <div className="upper-section-text-container" data-aos="fade-up"><h1>Want to make some <span>money</span> with us?</h1>
+            <h2>Or just switching tasks with someone? It's time to become a Tasker!</h2></div>
     </section>
 
 
-    const third =  <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
-        <div className="upper-section-text-container" data-aos="fade-up"><h1>Welcome to <span>Third</span></h1>
-            <h2>Welcome to our webpage, where you can either buy or sell some time!</h2></div>
+    const third =  <section id="hero3" className="d-flex flex-column justify-content-center align-items-center">
+        <div className="upper-section-text-container" data-aos="fade-up"><h1>Or we can help you save some <span>time</span> for the family</h1>
+            <h2>Hi Buyer! Let our Taskers do your stuff, while you spend some some time with the family.</h2></div>
     </section>
 
     const colors = [first, second, third]
 
-    const delay = 2500;
+    const delay = 3000;
 
     const [index, setIndex] = React.useState(0);
     const timeoutRef = React.useRef(null);
@@ -33,7 +32,7 @@ function UpperSection() {
         }
     }
 
-    React.useEffect(() => {
+    useEffect(() => {
         resetTimeout();
         timeoutRef.current = setTimeout(
             () =>
