5029cacd4244945fc5d6ebf5e77560d499b26da7
takacsberni <takacsberni@gmail.com>
2022-08-03 16:42:30 +0200

    feat: change UserProfile - add some lombok annotations for shorter code

--- src/backend/main/java/com/codecool/timebuyers/model/UserProfile.java
+++ src/backend/main/java/com/codecool/timebuyers/model/UserProfile.java
@@ -1,6 +1,5 @@
 package com.codecool.timebuyers.model;
-
-import jdk.jfr.Enabled;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -8,6 +7,7 @@ import java.util.*;
 
 @Entity
 @NoArgsConstructor
+@Data
 public class UserProfile {
 
 
@@ -33,88 +33,6 @@ public class UserProfile {
     private List<Task> taskToTake;
     private int rating;
 
-    public UserProfile(String userName, String password,
-                       String phoneNumber, String email, String town,
-                       UserStatus userStatus, List<Task> taskToNeed, List<Task> taskToTake, int rating) {
-        this.id = UUID.randomUUID();
-        this.userName = userName;
-        this.password = password;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.town = town;
-        this.userStatus = userStatus;
-        this.taskToNeed = taskToNeed;
-        this.taskToTake = taskToTake;
-        this.rating = rating;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public UserStatus getUserStatus() {
-        return userStatus;
-    }
-
-    public String getTown() {
-        return town;
-    }
-
-    public List<Task> getTaskToNeed() {
-        return taskToNeed;
-    }
-
-    public List<Task> getTaskToTake() {
-        return taskToTake;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setTown(String town) {
-        this.town = town;
-    }
-
-    public void setUserStatus(UserStatus userStatus) {
-        this.userStatus = userStatus;
-    }
-
-    public void setTaskToNeed(List<Task> taskToNeed) {
-        this.taskToNeed = taskToNeed;
-    }
-
-    public void setTaskToTake(List<Task> taskToTake) {
-        this.taskToTake = taskToTake;
-    }
-
-    public void addTaskToNeed(Task task){
-        taskToNeed.add(task);
-    }
-
-    public void addTaskToTake(Task task){
-        taskToTake.add(task);
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
 
     @Override
     public String toString() {
